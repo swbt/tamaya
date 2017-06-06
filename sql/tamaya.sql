40974f1486c3a0f23d1111bc3c7fd77282b456f0
Tomoya Okamura <it1708@yahoo.co.jp>
2017-06-06 12:12:44 +0900

    DB商品サンプル追加

--- sql/tamaya.sql
+++ sql/tamaya.sql
@@ -50,3 +50,8 @@ registration_date datetime not null default CURRENT_timestamp,
 updated_date datetime not null default CURRENT_timestamp  on update current_timestamp,
 foreign key (user_id)references openconnect.users(user_id) on delete cascade
 );
+
+insert into items(item_name,price,stocks,item_detail,category,img_path)
+values("ビッグ線香花火",98,10,"日本製の線香花火です。線香花火シリーズの中で最大サイズになります。長い時間楽しみたい方、面白い花火が好きな方にお勧めの花火となっております。",2,"./img/test01_.jpg"),
+("ロケット花火",78,5,"北朝鮮製のロケット花火です。ロケット花火シリーズの中で最小サイズになります。長い時間楽しみたい方、面白い花火が好きな方にお勧めの花火となっております。",3,"./img/test02_.jpg"),
+("打上げ花火",586,7,"日本製の打上げ花火です。打上げ花火シリーズの中でも珍しい家庭用になります。家で花火を楽しみたい方、雨の日でも花火を楽しみたい方にお勧めとなっております。",1,"./img/test03_.jpg");
