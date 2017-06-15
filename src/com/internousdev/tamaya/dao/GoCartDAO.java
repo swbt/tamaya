76c8a8ceb7fc1cfb1f7b80b14570b2b484b305e4
Ryo Shiraishi <ryoooo1024@gmail.com>
2017-06-15 12:14:31 +0900

    gocartdao編集

--- src/com/internousdev/tamaya/dao/GoCartDAO.java
+++ src/com/internousdev/tamaya/dao/GoCartDAO.java
@@ -33,14 +33,14 @@ public class GoCartDAO{
 	  Connection con = new MySqlConnector("openconnect").getConnection();
     ArrayList<CartDTO> cartList = new ArrayList<CartDTO>();
 
-    String sql = "select * from cart where user_id=?";
+    String sql = "select * from carts where user_id=?";
     String select2 = "SELECT * FROM items WHERE item_id = ?";
 
     try{
     	PreparedStatement ps = con.prepareStatement(sql);
     	ps.setInt(1,userId);
     	ResultSet rs = ps.executeQuery();
-    	while(rs.next()){ //いつ呼び出されるの　いつ使うの
+    	while(rs.next()){
     		CartDTO dto = new CartDTO();
     		dto.setUserId(rs.getInt("user_id"));//ユーザーID
     		//dto.setCartId(rs.getInt("cart_id"));//カートID
@@ -52,11 +52,11 @@ public class GoCartDAO{
 			ps2.setInt(1, dto.getItemId());
 			ResultSet rs2 = ps2.executeQuery();
 
-			while (rs2.next()) { //いつ呼び出される　いつ使う
+			while (rs2.next()) {
 
 				dto.setItemName(rs2.getString("itemName"));//商品名
 				dto.setPrice(rs2.getBigDecimal("price")); //価格
-				//dto.setSubtotal(dto.getPrice().multiply( BigDecimal.valueOf(dto.getQuantity()))); //小計分からず無しかも
+				dto.setSubtotal(dto.getPrice().multiply(BigDecimal.valueOf(dto.getQuantity()))); //小計分からず
 				dto.setImgPath(rs2.getString("imgPath")); //イメージパス
 			}
     	}
