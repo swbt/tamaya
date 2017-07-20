a32050e173a96d2e16813e4fd6631fae8ba25407
Kaori_Takahashi <internousdev@00174045119440>
2017-07-20 12:03:46 +0900

    コミットテスト。

---
+++ src/com/internousdev/tamaya/util/Definition.java
@@ -0,0 +1,22 @@
+package com.internousdev.tamaya.util;
+/**
+ * ネットワークネームを決定するクラス
+ * @author Kaori Takahashi
+ * @since 2017/7/20
+ * @version 1.0
+ */
+public class Definition {
+	/**
+	* ネットワークネーム(Twitter)
+	*/
+	public static final int NETWORK_NAME_TWITTER = 1; //twitter
+	/**
+	* ネットワークネーム(Facebook)
+	*/
+	public static final int NETWORK_NAME_FACEBOOK = 2; //facebook
+	/**
+	* ネットワークネーム(Google)
+	*/
+	public static final int NETWORK_NAME_GOOGLE = 3; //google
+
+}
