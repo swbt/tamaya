514ecccc60ad82fc57129528bb991c016d416c0a
Junya Takeno <tj8627@gmail.com>
2017-07-21 15:44:48 +0900

    push

---
+++ src/com/internousdev/tamaya/dto/test/MypageDTO.java
@@ -0,0 +1,244 @@
+
+
+
+package com.internousdev.tamaya.dto.test;
+/**
+ * 顧客情報に関するDTOクラス
+ *
+ * @author　YUKO TSUJI
+ * @since
+ * @version 1.0
+ */
+public class MypageDTO {
+	/**
+	 * ユーザーID
+	 */
+	private int userId;
+
+	/**
+	 * パスワード
+	 */
+	private String password;
+
+	private String email;
+
+
+	/**
+	 * 姓(英語)
+	 */
+	private String familyName;
+
+
+	/**
+	 * 名(英語)
+	 */
+	private String givenName;
+
+	/**
+	 * 姓(漢字)
+	 */
+	private String familyNameKanji;
+
+	/**
+	 * 姓(ひらがな)
+	 */
+	private String familyNameKana;
+
+	/**
+	 * 名(漢字)
+	 */
+	private String givenNameKanji;
+
+	/**
+	 * 名(ひらがな)
+	 */
+	private String givenNameKana;
+
+	/**
+	 * 郵便番号
+	 */
+	private String postal;
+
+	/**
+	 * 住所
+	 */
+	private String address;
+
+	/**
+	 * ログインフラグ
+	 */
+	private boolean loginFlg;
+
+	/**
+	 * @return userId
+	 */
+	public int getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId セットする userId
+	 */
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password セットする password
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email セットする email
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return familyName
+	 */
+	public String getFamilyName() {
+		return familyName;
+	}
+
+	/**
+	 * @param familyName セットする familyName
+	 */
+	public void setFamilyName(String familyName) {
+		this.familyName = familyName;
+	}
+
+	/**
+	 * @return givenName
+	 */
+	public String getGivenName() {
+		return givenName;
+	}
+
+	/**
+	 * @param givenName セットする givenName
+	 */
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+
+	/**
+	 * @return familyNameKanji
+	 */
+	public String getFamilyNameKanji() {
+		return familyNameKanji;
+	}
+
+	/**
+	 * @param familyNameKanji セットする familyNameKanji
+	 */
+	public void setFamilyNameKanji(String familyNameKanji) {
+		this.familyNameKanji = familyNameKanji;
+	}
+
+	/**
+	 * @return familyNameKana
+	 */
+	public String getFamilyNameKana() {
+		return familyNameKana;
+	}
+
+	/**
+	 * @param familyNameKana セットする familyNameKana
+	 */
+	public void setFamilyNameKana(String familyNameKana) {
+		this.familyNameKana = familyNameKana;
+	}
+
+	/**
+	 * @return givenNameKanji
+	 */
+	public String getGivenNameKanji() {
+		return givenNameKanji;
+	}
+
+	/**
+	 * @param givenNameKanji セットする givenNameKanji
+	 */
+	public void setGivenNameKanji(String givenNameKanji) {
+		this.givenNameKanji = givenNameKanji;
+	}
+
+	/**
+	 * @return givenNameKana
+	 */
+	public String getGivenNameKana() {
+		return givenNameKana;
+	}
+
+	/**
+	 * @param givenNameKana セットする givenNameKana
+	 */
+	public void setGivenNameKana(String givenNameKana) {
+		this.givenNameKana = givenNameKana;
+	}
+
+	/**
+	 * @return postal
+	 */
+	public String getPostal() {
+		return postal;
+	}
+
+	/**
+	 * @param postal セットする postal
+	 */
+	public void setPostal(String postal) {
+		this.postal = postal;
+	}
+
+	/**
+	 * @return address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address セットする address
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return loginFlg
+	 */
+	public boolean isLoginFlg() {
+		return loginFlg;
+	}
+
+	/**
+	 * @param loginFlg セットする loginFlg
+	 */
+	public void setLoginFlg(boolean loginFlg) {
+		this.loginFlg = loginFlg;
+	}
+
+
+
+
+
+}
